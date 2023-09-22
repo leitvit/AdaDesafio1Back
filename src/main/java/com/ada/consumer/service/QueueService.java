158a6c36ee8af3f8ddd296e3d88a5a43f03a62d5
Joás de Brito Ferreira Filho <appsec.joas@gmail.com>
2023-09-22 20:34:37 -0300

    Update QueueService.java
    implement get all information method
    To-Do: refinement on the processing mechanism

--- src/main/java/com/ada/consumer/service/QueueService.java
+++ src/main/java/com/ada/consumer/service/QueueService.java
@@ -4,6 +4,12 @@ import com.ada.consumer.model.ConsumerFeedback;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import software.amazon.awssdk.services.sqs.SqsClient;
+import software.amazon.awssdk.services.sqs.model.Message;
+import software.amazon.awssdk.services.sqs.model.ReceiveMessageRequest;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
 
 @Service
 public class QueueService {
@@ -14,5 +20,22 @@ public class QueueService {
     public Long getQueueSizeForType(ConsumerFeedback.FeedbackType feedbackType) {
         sqsClient.listQueues();//TODO implement logic
         return 1L;
+    }
+
+    public List<List<Message>> getAllInformation() {
+        // here we must poll the three queues and return the parsed content summarized
+        List<software.amazon.awssdk.services.sqs.model.Message> messages = new ArrayList<>();
+        List<List<software.amazon.awssdk.services.sqs.model.Message>> allinfo = new ArrayList<List<software.amazon.awssdk.services.sqs.model.Message>>();
+        List<String> URLs = Arrays.asList("https://sqs.us-east-1.amazonaws.com/908737804402/criticas.fifo", "https://sqs.us-east-1.amazonaws.com/908737804402/elogios.fifo", "https://sqs.us-east-1.amazonaws.com/908737804402/sugestoes.fifo");
+        for(String s: URLs) {
+            ReceiveMessageRequest receiveMessageRequest = ReceiveMessageRequest.builder()
+                    .queueUrl(s)
+                    .build();
+            messages = sqsClient.receiveMessage(receiveMessageRequest).messages();
+            allinfo.add(messages);
+            System.out.println(messages);
+            System.out.println("\n");
+        }
+        return allinfo;
     }
 }
