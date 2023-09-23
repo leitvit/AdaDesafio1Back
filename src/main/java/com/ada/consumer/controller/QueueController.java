f01efd5a64d133d81e3f591c5d378c23a455d9d8
Joás de Brito Ferreira Filho <appsec.joas@gmail.com>
2023-09-22 21:41:28 -0300

    Revert "Consumer"

--- src/main/java/com/ada/consumer/controller/QueueController.java
+++ src/main/java/com/ada/consumer/controller/QueueController.java
@@ -8,9 +8,6 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
-import java.util.HashMap;
-import java.util.Map;
-
 @Controller
 public class QueueController {
 
@@ -23,17 +20,6 @@ public class QueueController {
             return ResponseEntity.ok(queueService.getQueueSizeForType(feedbackType));
         } catch(Exception ex) {
             return ResponseEntity.internalServerError().body("Error communicating with SQS.");
-        }
-    }
-
-    @GetMapping("v1/queue-info")
-    public ResponseEntity getQueueInfo() {
-        try {
-            var information = queueService.getAllInformation();
-            System.out.println(information);
-            return ResponseEntity.ok(information);
-        } catch(Exception ex) {
-            return ResponseEntity.internalServerError().body("Could not retrieve information: Error communicating with SQS.");
         }
     }
 }
--- src/main/java/com/ada/consumer/service/QueueService.java
+++ src/main/java/com/ada/consumer/service/QueueService.java
@@ -5,18 +5,12 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
 import software.amazon.awssdk.services.sqs.SqsClient;
-
-import software.amazon.awssdk.services.sqs.model.Message;
-import software.amazon.awssdk.services.sqs.model.ReceiveMessageRequest;
-import java.util.*;
-
 import software.amazon.awssdk.services.sqs.model.GetQueueAttributesRequest;
 import software.amazon.awssdk.services.sqs.model.GetQueueAttributesResponse;
 import software.amazon.awssdk.services.sqs.model.QueueAttributeName;
 
 import javax.annotation.PostConstruct;
 import java.util.HashMap;
-
 
 @Service
 public class QueueService {
@@ -49,38 +43,5 @@ public class QueueService {
 
         GetQueueAttributesResponse attributesResponse = sqsClient.getQueueAttributes(attributesRequest);
         return Integer.parseInt(attributesResponse.attributes().get(QueueAttributeName.APPROXIMATE_NUMBER_OF_MESSAGES));
-    }
-
-    public Map<String, String> getAllInformation() {
-        // here we must poll the three queues and return the parsed content summarized
-        List<String> readable_results = new ArrayList<String>();
-        List<software.amazon.awssdk.services.sqs.model.Message> messages = new ArrayList<>();
-        List<List<software.amazon.awssdk.services.sqs.model.Message>> allinfo = new ArrayList<List<software.amazon.awssdk.services.sqs.model.Message>>();
-        List<String> URLs = Arrays.asList(
-                "https://sqs.us-east-1.amazonaws.com/908737804402/criticas.fifo",
-                "https://sqs.us-east-1.amazonaws.com/908737804402/elogios.fifo",
-                "https://sqs.us-east-1.amazonaws.com/908737804402/sugestoes.fifo");
-        String readable_aux = "";
-        String key = "";
-        List<String> keys = new ArrayList<String>();
-        keys.add("criticas"); keys.add("elogios"); keys.add("sugestoes");
-        Map<String, String> dict = new HashMap<String, String>();
-
-        for(String s: URLs) {
-            ReceiveMessageRequest receiveMessageRequest = ReceiveMessageRequest.builder()
-                    .queueUrl(s)
-                    .build();
-            messages = sqsClient.receiveMessage(receiveMessageRequest).messages();
-            allinfo.add(messages);
-        }
-        for(int i=0; i<3; i++) {
-            readable_aux = "";
-            for(Message m: allinfo.get(i)){
-                key = keys.get(1);
-                readable_aux += m.toString() + "\n";
-            }
-            dict.put(key, readable_aux);
-        }
-        return dict;
     }
 }
