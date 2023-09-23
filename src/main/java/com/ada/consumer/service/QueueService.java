8fb2a7bdf56078e1854e22f5a80ca9c86fbe60f9
Joás de Brito Ferreira Filho <appsec.joas@gmail.com>
2023-09-22 21:37:04 -0300

    Update QueueService.java
    change data type and structure returned by queues querying

--- src/main/java/com/ada/consumer/service/QueueService.java
+++ src/main/java/com/ada/consumer/service/QueueService.java
@@ -7,9 +7,7 @@ import software.amazon.awssdk.services.sqs.SqsClient;
 import software.amazon.awssdk.services.sqs.model.Message;
 import software.amazon.awssdk.services.sqs.model.ReceiveMessageRequest;
 
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.List;
+import java.util.*;
 
 @Service
 public class QueueService {
@@ -22,20 +20,36 @@ public class QueueService {
         return 1L;
     }
 
-    public List<List<Message>> getAllInformation() {
+    public Map<String, String> getAllInformation() {
         // here we must poll the three queues and return the parsed content summarized
+        List<String> readable_results = new ArrayList<String>();
         List<software.amazon.awssdk.services.sqs.model.Message> messages = new ArrayList<>();
         List<List<software.amazon.awssdk.services.sqs.model.Message>> allinfo = new ArrayList<List<software.amazon.awssdk.services.sqs.model.Message>>();
-        List<String> URLs = Arrays.asList("https://sqs.us-east-1.amazonaws.com/908737804402/criticas.fifo", "https://sqs.us-east-1.amazonaws.com/908737804402/elogios.fifo", "https://sqs.us-east-1.amazonaws.com/908737804402/sugestoes.fifo");
+        List<String> URLs = Arrays.asList(
+                "https://sqs.us-east-1.amazonaws.com/908737804402/criticas.fifo",
+                "https://sqs.us-east-1.amazonaws.com/908737804402/elogios.fifo",
+                "https://sqs.us-east-1.amazonaws.com/908737804402/sugestoes.fifo");
+        String readable_aux = "";
+        String key = "";
+        List<String> keys = new ArrayList<String>();
+        keys.add("criticas"); keys.add("elogios"); keys.add("sugestoes");
+        Map<String, String> dict = new HashMap<String, String>();
+
         for(String s: URLs) {
             ReceiveMessageRequest receiveMessageRequest = ReceiveMessageRequest.builder()
                     .queueUrl(s)
                     .build();
             messages = sqsClient.receiveMessage(receiveMessageRequest).messages();
             allinfo.add(messages);
-            System.out.println(messages);
-            System.out.println("\n");
         }
-        return allinfo;
+        for(int i=0; i<3; i++) {
+            readable_aux = "";
+            for(Message m: allinfo.get(i)){
+                key = keys.get(1);
+                readable_aux += m.toString() + "\n";
+            }
+            dict.put(key, readable_aux);
+        }
+        return dict;
     }
 }
