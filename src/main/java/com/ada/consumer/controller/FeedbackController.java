81e8f487d500c62381904dc4fe92b5d77ae84d2b
alissao <alissonhbv@gmail.com>
2023-09-25 16:01:01 -0300

    Adding permissive CORS for developing purposes.

--- src/main/java/com/ada/consumer/controller/FeedbackController.java
+++ src/main/java/com/ada/consumer/controller/FeedbackController.java
@@ -11,12 +11,10 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.validation.annotation.Validated;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
+@CrossOrigin(origins = "*", allowedHeaders = "*")
 @RequestMapping(value = "v1", produces = "text/plain")
 @Tag(name = "bootcampback")
 public class FeedbackController {
--- src/main/java/com/ada/consumer/controller/QueueController.java
+++ src/main/java/com/ada/consumer/controller/QueueController.java
@@ -12,11 +12,13 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
+@CrossOrigin(origins = "*", allowedHeaders = "*")
 @RequestMapping(produces = "application/json")
 @Tag(name = "bootcampback")
 public class QueueController {
